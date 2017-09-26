4138ce83d85c980c6017f408beb945301b4e2384
HyunChul Lee <hc89@naver.com>
2017-09-26 15:04:25 +0900

    Add OpenStream line

--- src/Network/ParseURL.java
+++ src/Network/ParseURL.java
@@ -1,5 +1,8 @@
 package Network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,9 +19,18 @@ public class ParseURL {
 			System.out.printf("Host : %s\n", opinion.getHost());
 			System.out.printf("Port : %s\n", opinion.getPort());
 			System.out.printf("Path : %s\n", opinion.getPath());
-			System.out.printf("File : %s\n", opinion.getFile());			
+			System.out.printf("File : %s\n", opinion.getFile());
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(opinion.openStream()));
+			String line = null;
+			
+			while((line = br.readLine()) != null)
+				System.out.println(line);
+			br.close();
 		} catch (MalformedURLException e) {
 			System.err.println("잘못된 URL 주소입니다.");
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
